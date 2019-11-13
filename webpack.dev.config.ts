5f484026d683fe85becd5b16d6859476bea2e6c1
Martin <imbedoya@tekmexico.com>
2019-11-13 11:50:52 -0800

    Configuration file changed by a ts file

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,12 +1,47 @@
 import path from 'path';
 import webpack from 'webpack';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import {} from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import 'webpack-dev-server';
 
 const config:webpack.Configuration = {
     mode: "development",
     entry: './client/src/index.tsx',
     plugins: [
-
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './client/template/index.html'
+        }),
     ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, "./client/public/"),
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                },
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, "./client/public/"),
+    },
 };
+
+export default config;
