1e6ce9d1623a7b5675aeb7f150d48f93828f6d43
Martin <imbedoya@tekmexico.com>
2019-11-28 17:37:18 -0800

    GraphQLServer configured to communicate with the browser through https, resolvers configured with a unit testing variable, beginning the validation part

--- userServer/src/index.ts
+++ userServer/src/index.ts
@@ -1,8 +1,18 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, Options } from 'graphql-yoga';
 import { resolvers } from './resolvers';
+import fs from 'fs';
+import path from 'path';
 
 const server = new GraphQLServer({
   typeDefs: './userServer/src/schema.graphql',
   resolvers,
 })
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+
+const options:Options = {
+  https: {
+    key: fs.readFileSync(path.resolve(__dirname,'../keys/key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname,'../keys/certificate.pem')),
+  },
+};
+
+server.start(options,() => console.log(`Server is running on https://localhost:4000`))
