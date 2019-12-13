e59d88e722bce2b715c6ac43808ad79565f5d30c
Martin <imbedoya@tekmexico.com>
2019-12-12 16:48:48 -0800

    Basic test of socket.io for notifications

--- Messages/src/index.ts
+++ Messages/src/index.ts
@@ -1,3 +1,22 @@
 import { GraphQLServer } from 'graphql-yoga'
 import socket from 'socket.io';
+import express from 'express';
+import https from 'https';
 
+const app = express();
+const httpsOptions:https.ServerOptions = {
+    key: "./Messages/keys/key.pem",
+    cert: "./Messages/keys/certificate.pem"
+};
+const server = https.createServer(httpsOptions,app);
+
+const ioOptions:socket.ServerOptions = {
+    path: "/listen"
+};
+const io = socket(server);
+
+io.on("connection",function(socket){
+    socket.on("notification connected",function(msg){
+        console.log(msg);
+    });
+});
