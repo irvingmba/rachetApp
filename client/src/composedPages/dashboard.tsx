a5542b8b1d89260f28dcceae4e0014eff2c66252
Martin <imbedoya@tekmexico.com>
2020-01-14 17:39:08 -0800

    I made some modifications to check if the router could render some nested components, I made a contacts section and I will make a conversations and profile sections in the src of the client project to render them in the dashboard components

--- client/src/composedPages/dashboard.tsx
+++ client/src/composedPages/dashboard.tsx
@@ -3,9 +3,10 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import DashboardMenu from "../composedBlocks/dashboard/dashMenu";
-import { Route, Switch } from "react-router-dom";
-import ContactList from '../composedBlocks/dashboard/contacts';
-import { PATH_DASHBOARD_CONTACTS_V } from '../globalConfig';
+import { Route, Switch, NavLink } from "react-router-dom";
+import ContactList from '../composedBlocks/contacts/contacts';
+import { PATH_DASHBOARD_CONTACTS_V, PATH_DASH_CONT_DISPLAY_V } from '../globalConfig';
+import DashDisplay from '../composedBlocks/dashboard/dashDisplay';
 
 const DashboardPage = () => {
   return (<>
@@ -20,11 +21,17 @@ const DashboardPage = () => {
           <Switch>
             <Route path={PATH_DASHBOARD_CONTACTS_V}>
               <h5>Contact list</h5>
+              <NavLink to={PATH_DASH_CONT_DISPLAY_V}>Display</NavLink>
             </Route>
           </Switch>
         </Col>
         <Col>
           <h3>Display</h3>
+          <Switch>
+            <Route path={PATH_DASH_CONT_DISPLAY_V}>
+              <DashDisplay />
+            </Route>
+          </Switch>
         </Col>
       </Row>
     </Container>
--- client/src/globalConfig.ts
+++ client/src/globalConfig.ts
@@ -6,6 +6,7 @@ export const PATH_LOGIN_VIEW = "/";
 export const PATH_REGISTER_VIEW = "/registry";
 export const PATH_DASHBOARD_VIEW = "/dashboard";
 export const PATH_DASHBOARD_CONTACTS_V = PATH_DASHBOARD_VIEW + "/contacts";
+export const PATH_DASH_CONT_DISPLAY_V = PATH_DASHBOARD_CONTACTS_V + "/display";
 
 // Paths for the requests from the components of the application
 export const URL_LOGIN = "/login"
