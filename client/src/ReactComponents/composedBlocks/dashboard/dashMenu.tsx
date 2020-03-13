206c8f3c14f3de8d05589d048276d7b501a9a790
Martin <imbedoya@tekmexico.com>
2020-03-12 17:43:36 -0700

    Implementing material ui on the dashboard and implementing polyglot for internationalization

--- client/src/ReactComponents/composedBlocks/dashboard/dashMenu.tsx
+++ client/src/ReactComponents/composedBlocks/dashboard/dashMenu.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { PATH_DASHBOARD_CONTACTS_V } from '../../../globalConfig';
-
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import Conversations from "@material-ui/icons/Forum";
+import Friends from "@material-ui/icons/Contacts";
 
 const DashboardMenu = () => {
   return (
     <>
-    <ul>
-      <li>
-        <NavLink to={PATH_DASHBOARD_CONTACTS_V}>Contacts</NavLink>
-      </li>
-    </ul>
+    <div>
+    <NavLink to={PATH_DASHBOARD_CONTACTS_V}>
+    <ListItem button>
+      <ListItemIcon>
+        <Conversations />
+      </ListItemIcon>
+      <ListItemText primary="Messages" />
+    </ListItem>
+    </NavLink>
+    <ListItem button>
+      <ListItemIcon>
+        <Friends />
+      </ListItemIcon>
+      <ListItemText primary="Friends" />
+    </ListItem>
+    </div>
     </>
   );
 };
--- client/src/ReactComponents/composedPages/dashboard/dashboard.tsx
+++ client/src/ReactComponents/composedPages/dashboard/dashboard.tsx
@@ -6,8 +6,23 @@ import DashDisplay from '../../composedBlocks/dashboard/dashDisplay';
 import DashOptions from '../../composedBlocks/dashboard/dashOptions';
 import { connect, useDispatch } from 'react-redux';
 import { asyncGetContacts, asyncSocketInit } from "../../../StateManagement/reduxSaga/asyncActions";
+import { dashbStyles } from "./styles";
+import { CssBaseline, AppBar, Toolbar, IconButton, Typography, Badge, Drawer, Divider, List } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import clsx from "clsx";
 
 const DashboardPage = () => {
+  const styles = dashbStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  const fixedHeightPaper = clsx(styles.paper, styles.fixedHeight);
 
   // Call an asynchronous method to restore the state of the application
   const dispatch = useDispatch();
@@ -15,8 +30,49 @@ const DashboardPage = () => {
   dispatch(asyncSocketInit());
 
   return (<>
+  <div className={styles.root}>
+  <CssBaseline />
+      <AppBar position="absolute" className={clsx(styles.appBar, open && styles.appBarShift)}>
+        <Toolbar className={styles.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(styles.menuButton, open && styles.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={styles.title}>
+            Dashboard
+          </Typography>
+          <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(styles.drawerPaper, !open && styles.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={styles.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List><DashboardMenu /></List>
+        <Divider />
+        {/* <List>{secondaryListItems}</List> */}
+      </Drawer>
+  </div>
     <h3>Menu</h3>
-    <DashboardMenu />
+    {/* <DashboardMenu /> */}
     <DashOptions />
     <DashDisplay />
     </>
